112de0933525461a337c93ca2adf10fc6ba0cc06
lokithe5th <lourenslinde@gmail.com>
2023-06-28 10:02:52 +0200

    Started SVG + strings

--- src/TokenSwap.sol
+++ src/TokenSwap.sol
@@ -4,40 +4,93 @@ pragma solidity 0.8.19;
 
 import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/utils/Strings.sol";
 
-contract TokenShop is ERC721 {
+/// @title TokenSwap
+/// @author lourens
+/// @notice Permissionless fixed-price market maker for ERC20 tokens that crashed in value
+
+contract TokenSwap is ERC721 {
+    using Strings for uint256;
+    /// Target is the address to which market creation fees go
     address public target;
+    /// Address nominated to take over collection of market creation fees
     address public pendingTarget;
+    /// Amount of ether accrued to the `target`
     uint256 public targetFunds;
 
+    /// The amount of markets created
     uint256 public supply;
 
-    uint256 public constant ACCESS_COST = 0.005 ether;
-    uint256 public constant USE_COST = 0.001 ether;
-    address public constant SENTINEL = address(1);
+    string[] private template = [
+        "TokenSwap Invoice",
+        "INV-",
+        "Seller",
+        "Token",
+        "Amount",
+        "Paid"
+    ];
 
+    string[10] internal svgParts = [
+        '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 400"><style>.base { fill: white; font-family: monospace; font-size: 14px; }</style><rect width="100%" height="100%" fill="black" /><text x="10" y="20" class="base">',
+        '</text><text x="10" y="40" class="base">',
+        '</text><text x="10" y="60" class="base">',
+        '</text><text x="10" y="80" class="base">',
+        '</text><text x="10" y="100" class="base">',
+        '</text><text x="10" y="120" class="base">',
+//        '</text><text x="10" y="140" class="base">',
+//        '</text><text x="10" y="160" class="base">',
+//        '</text><text x="10" y="160" class="base">',
+        '</text></svg>'
+    ];
+
+    /****************************************************************
+     *                  CONSTANTS                                   *
+     ****************************************************************/
+
+    /// The cost to gain access to TokenSwap
+    uint256 public constant ACCESS_COST = 0.005 ether;
+    /// The cost to call `createMarket` and `sellTokens`
+    /// @dev this `USE_COST` is deducted from the callers internal `accounts[caller]`
+    uint256 public constant USE_COST = 0.001 ether;
+
+    /****************************************************************
+     *                  ERRORS                                      *
+     ****************************************************************/
+    /// `msg.value` is too low
     error InsufficientValue();
+    /// The market already exists
     error Exists();
+    /// Invoice has not been issued yet
+    error Invalid();
+    /// The internal account balance is too low for requested action
     error NoFunds();
+    /// No market has been created for the selected token
     error NoMarket();
+    /// Please donate direct deposits to buidlguidl.eth
     error NotAllowed();
+    /// The requested token transfer was unsuccessful
     error NoTransfer();
+    /// Exactly what is says :-\
     error Unauthorized();
 
+    /// Holds data from which invoice SVG can be built
+    /// We exclude `cost` here because the price offered is always `USE_COST`
     struct Invoice {
-        address seller;
-        address token;
-        uint256 amountOfTokens;
-        uint256 cost;
+        address seller; /// the account selling the tokens
+        address token; /// the token that was sold
+        uint256 amountOfTokens; /// the amount of tokens sold
     }
 
+    /// Accounts to deposits
     mapping(address => uint256) public accounts;
+    /// Is a market available for trading?
     mapping(address => bool) public markets;
+    /// TokenId to Invoice
     mapping(uint256 => Invoice) public invoices;
 
-    constructor(address _target) ERC721("TokenShop: Invoice", "TSI") {
+    constructor(address _target) ERC721("TokenSwap: Invoice", "TSI") {
         target = _target;
-        markets[SENTINEL] = SENTINEL;
     }
 
     /****************************************************************
@@ -92,11 +145,36 @@ contract TokenShop is ERC721 {
     }
 
     /****************************************************************
-     *                  TO DO: SVG INVOICE VIEWERS
-     ***************************************************************/
+     *                  WIP: SVG INVOICE VIEWERS                  *
+     ****************************************************************/
+    function tokenURI(uint256 tokenId) public view override returns (string memory) {
+        _requireMinted(tokenId);
+
+      string memory json = Base64.encode(
+        bytes(string.concat(
+          '{"name": "Invoice #', uint2str(id), ' "description": "TokenSwap Invoice", "image": "data:image/svg+xml;base64,', Base64.encode(bytes(generateSVGofTokenById(id))), '"}'))));
+
+        json = string.concat('data:application/json;base64,', json);
+      return json;
+    }
+
+    /// @notice Public facing render function
+    /// @dev    Visibility is `public` to enable it being called by other contracts for composition.
+    /// @param  id Target for rendering
+    /// @return string SVG image represented as a string
+    function renderTokenById(uint256 id) public view returns (string memory) {
+        if (!_exists(id)) revert Invalid();
+        return string(abi.encodePacked(generateSVGofTokenById(id)));
+    }
+
+    function generateSVGofTokenById(uint256 id) public view returns (string memory) {
+        string memory svg = string.concat(svgParts[0], template[0], svgParts[1], template[1], id.toString(), svgParts[2], template[2], toHexString(invoices[id].seller), svgParts[3], template[3], toHexString(invoices[id].token), svgParts[4]);
+        svg = string.concat(svg, template[4], toString(invoices[id].amountOfTokens), svgParts[5], template[5], "0.001 ether", svgParts[9]);
+        return svg;
+    }
 
     /****************************************************************
-     *                  MANAGEMENT FUNCTIONS
+     *                  MANAGEMENT FUNCTIONS                        *
      ****************************************************************/
 
     /// @notice Allows a beneficiary organization to transfer out tokens
@@ -120,7 +198,7 @@ contract TokenShop is ERC721 {
     }
 
     /****************************************************************
-     *                  BENEFICIARY SETTERS
+     *                  BENEFICIARY SETTERS                         *
      ****************************************************************/
 
     /// @notice Nominates a new beneficiary
